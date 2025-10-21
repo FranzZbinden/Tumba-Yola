375ead76b7322398ea255aaf21e4c7ab5e23ba2c
Franz <franz.zbinden7@gmail.com>
2025-10-20 23:22:53 -0400

    Connected network with server.

--- network.py
+++ network.py
@@ -4,11 +4,11 @@ import os
 
 load_dotenv()
 
-ip = os.dotenv("IP")
+ip = os.getenv("IP")
 
 class Network:
-    def _init_(self):
-        self.client = socket.socket(socket.IF_INET, socket.SOCK_STREAM)
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = ip
         self.port = 5555
         self.addr = (self.server, self.port)
@@ -22,3 +22,4 @@ class Network:
         except: 
             pass
 
+n = Network()
